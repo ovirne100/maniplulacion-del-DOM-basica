bc9e6efafd7aef9aebb5fbc68783e32aec3efb62
ovirne balanta <luzovirnebalanta11@gmail.com>
2022-09-12 12:27:23 -0500

    agregando .gitignore al archivo del proyecto

---
+++ scrip.js
@@ -0,0 +1,9 @@
+//let form = document.querySelector(".form");
+let input = document.querySelector("#calculo1");
+let input = document.querySelector("#calculo2");
+let btn = document.querySelector("btncalcular");
+let pResult = document.querySelector("#result")
+
+function btnonclick(){
+  console.log()
+}
