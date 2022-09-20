fcfb12a3ddd9a906dfde4596c6b52d8071c298d2
ovirne balanta <luzovirnebalanta11@gmail.com>
2022-09-20 17:01:39 -0500

    my primer proyecto hecho por my html,css,js

--- scrip.js
+++ scrip.js
@@ -1,9 +1,34 @@
-//let form = document.querySelector(".form");
-let input = document.querySelector("#calculo1");
-let input = document.querySelector("#calculo2");
-let btn = document.querySelector("btncalcular");
-let pResult = document.querySelector("#result")
+let carro1 = document.querySelector('.car1')
+let carro2 = document.querySelector('.car2')
 
-function btnonclick(){
-  console.log()
+carro1.addEventListener('click',desaparececar2)
+
+function desaparececar2(){
+  carro2.classList.toggle('inactive')
+ 
 }
+
+
+
+let rayas3 = document.querySelector('.menu-hamburgueza')
+let deportivos = document.querySelector('.dpt')
+
+rayas3.addEventListener('click',cosasdeprtivas)
+
+function cosasdeprtivas (){
+  cocinaslindas.classList.add('inactive')
+deportivos.classList.toggle('inactive')
+}
+
+
+
+let carrito = document.querySelector('.shoppin-cart')
+let cocinaslindas = document.querySelector('.cocina')
+
+carrito.addEventListener('click',mycocina)
+
+function mycocina(){
+  deportivos.classList.add('inactive')
+  cocinaslindas.classList.toggle('inactive')
+}
+
